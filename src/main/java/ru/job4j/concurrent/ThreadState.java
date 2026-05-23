647e4008c0a0db192483c05c34ff318a038f6494
Timur <timk01@mail.ru>
2026-05-23 13:27:41 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,36 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    /**
+     * while (first.isAlive() || second.isAlive()) {
+     * }
+     * System.out.println("работа завершена");
+     * <p>
+     * можно бы было так (без дожйнов), логика:
+     * мы будем крутиться пока оба НЕ фолс (т.е. вылезем из цикла только когда оба мертвы)
+     * <p>
+     * -- ИЛИ можно прямо вообще в терминейтед упороться:
+     * while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+     * }
+     * System.out.println("работа завершена");
+     *
+     * @param args
+     * @throws InterruptedException
+     */
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
+        first.start();
+        second.start();
+
+        first.join();
+        second.join();
+        System.out.println("работа завершена");
+    }
+}
