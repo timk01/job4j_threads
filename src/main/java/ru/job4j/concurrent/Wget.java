da0905642c0d2617c3d21b7bd129fa46ae9287f9
Timur <timk01@mail.ru>
2026-05-23 14:02:07 +0300

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    for (int index = 0; index < 100; index++) {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.print("\rLoading : " + index  + "%");
+                    }
+                }
+        );
+        thread.start();
+    }
+}
