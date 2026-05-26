63c0b1674e43ddc5313384a1af0d797eae24e119
Timur <timk01@mail.ru>
2026-05-26 11:57:12 +0300

    0. Монитор, мьютекс, критическая секция [#6857 #564258]

---
+++ src/main/java/ru/job4j/concurrent/Count.java
@@ -0,0 +1,17 @@
+package ru.job4j.concurrent;
+
+public class Count {
+    private int value;
+
+    public void increment() {
+        synchronized (this) {
+            value++;
+        }
+    }
+
+    public int get() {
+        synchronized (this) {
+            return value;
+        }
+    }
+}
